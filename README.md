10a10ee7f58acd6797a068e84ca6f58bf11039f2
zack yin <youcancallmeallen@gmail.com>
2022-05-02 16:54:22 +0800

    feat:  add readme, add array tree nodes method

--- README.md
+++ README.md
@@ -11,8 +11,10 @@ stack
 
 tree
 - binary tree
+- trie
 
 graph
+
 
 
 ## algorithm
@@ -28,6 +30,16 @@ searching
 - binarySearch
 - BFS
 - DFS
+
+array manipulations
+- two pointers
+- sliding window
+
+linked list manipulations
+- two pointers
+- fast slow pointers
+
+
 
 ## algorithm categories (types)
 
@@ -90,6 +102,23 @@ If asked for common strings then
 Else
     - Map/Set for O(1) time & O(n) space
     - Sort input for O(nlogn) time and O(1) space
+```
+
+## 回溯(backtracking)
+
+```
+void backtracking(参数) {
+    if (终止条件) {
+        存放结果;
+        return;
+    }
+
+    for (选择：本层集合中元素（树中节点孩子的数量就是集合的大小）) {
+        处理节点;
+        backtracking(路径，选择列表); // 递归
+        回溯，撤销处理结果
+    }
+}
 ```
 
 ## leetcode patterns
--- data-structure/tree/binaryTree/BinaryTreeNode.js
+++ data-structure/tree/binaryTree/BinaryTreeNode.js
@@ -37,34 +37,48 @@ class BinaryTreeNode {
     return this
   }
 
-  get leftHeight(){
-    if(!this.left){
+  get leftHeight() {
+    if (!this.left) {
       return 0
     }
-    return this.left.height+1
+    return this.left.height + 1
   }
 
-  get rightHeight(){
-    if(!this.right){
+  get rightHeight() {
+    if (!this.right) {
       return 0
     }
-    return this.right.height+1
+    return this.right.height + 1
   }
 
-  get height(){
-    return Math.max(this.leftHeight,this.rightHeight)
+  get height() {
+    return Math.max(this.leftHeight, this.rightHeight)
   }
 
-  traverseInOrder(){
-    let traverse=[]
-    if(this.left){
+  traverseInOrder() {
+    let traverse = []
+    if (this.left) {
       traverse = traverse.concat(this.left.traverseInOrder())
     }
     traverse.push(this.value)
-    if(this.right){
+    if (this.right) {
       traverse = traverse.concat(this.right.traverseInOrder())
     }
     return traverse
+  }
+
+  static generateTreeFromArray(arr) {
+    return helper(arr, {}, 0)
+    // recursive helper
+    function helper(arr, node, index) {
+      if (index >= arr.length) return node
+      if (!arr[index]) return null
+      let temp = new BinaryTreeNode(arr[index])
+      node = temp
+      node.left = helper(arr, node.left, 2 * index + 1)
+      node.right = helper(arr, node.right, 2 * index + 2)
+      return node
+    }
   }
 }
 
